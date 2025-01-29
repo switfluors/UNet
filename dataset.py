Updated testing code (still need to fix normalization)

dataset.py
@@ -20,25 +20,24 @@ def get_train_test_datasets():
     test_dataset_notnorm = None
 
     with h5py.File(config.TRAINING_DATA_PATH) as matdata_train:
+
+        sptimg4_train_notnorm = matdata_train['sptimgPerlin'][:]
+        tbg4_train_notnorm = matdata_train['tbgPerlin'][:]
+
         if config.NORMALIZATION:
             sptimg4_train_norm = matdata_train['normalized_sptimgPerlin'][:]
-            sptimg4_train_notnorm = matdata_train['sptimgPerlin'][:]
             tbg4_train_norm = matdata_train['normalized_tbgPerlin'][:]
-            tbg4_train_notnorm = matdata_train['tbgPerlin'][:]
-        else:
-            sptimg4_train_notnorm = matdata_train['sptimgPerlin'][:]
-            tbg4_train_notnorm = matdata_train['tbgPerlin'][:]
+
+    sptimg4_train_notnorm = np.expand_dims(sptimg4_train_notnorm, axis=1)
+    tbg4_train_notnorm = np.expand_dims(tbg4_train_notnorm, axis=1)
 
     if config.NORMALIZATION:
         sptimg4_train_norm = np.expand_dims(sptimg4_train_norm, axis=1)
-        sptimg4_train_notnorm = np.expand_dims(sptimg4_train_notnorm, axis=1)
         tbg4_train_norm = np.expand_dims(tbg4_train_norm, axis=1)
-        tbg4_train_notnorm = np.expand_dims(tbg4_train_notnorm, axis=1)
-    else:
-        sptimg4_train_notnorm = np.expand_dims(sptimg4_train_notnorm, axis=1)
-        tbg4_train_notnorm = np.expand_dims(tbg4_train_notnorm, axis=1)
+
 
     if config.TESTING_DATASET_SIZE == 0:
+
         if config.NORMALIZATION:
             data = CustomDataset(sptimg4_train_norm, tbg4_train_norm)
         else:
@@ -56,33 +55,34 @@ def get_train_test_datasets():
                 tbg4_test_norm = matdata['normalized_tbgPerlin'][:]
                 sptimg4_test_notnorm = matdata['sptimgPerlin'][:]
                 tbg4_test_notnorm = matdata['tbgPerlin'][:]
+                gt_spt_test = matdata['normalized_GTspt'][:]
             else:
                 sptimg4_test_notnorm = matdata['sptimgPerlin'][:]
                 tbg4_test_notnorm = matdata['tbgPerlin'][:]
+                gt_spt_test = matdata['GTspt'][:]
+
+        sptimg4_test_notnorm = np.expand_dims(sptimg4_test_notnorm, axis=1)
+        tbg4_test_notnorm = np.expand_dims(tbg4_test_notnorm, axis=1)
+        gt_spt_test = np.expand_dims(gt_spt_test, axis=1)
 
         if config.NORMALIZATION:
             sptimg4_test_norm = np.expand_dims(sptimg4_test_norm, axis=1)
             tbg4_test_norm = np.expand_dims(tbg4_test_norm, axis=1)
-            sptimg4_test_notnorm = np.expand_dims(sptimg4_test_notnorm, axis=1)
-            tbg4_test_notnorm = np.expand_dims(tbg4_test_notnorm, axis=1)
-        else:
-            sptimg4_test_notnorm = np.expand_dims(sptimg4_test_notnorm, axis=1)
-            tbg4_test_notnorm = np.expand_dims(tbg4_test_notnorm, axis=1)
 
         if config.NORMALIZATION:
             train_dataset = CustomDataset(sptimg4_train_norm, tbg4_train_norm)
             test_dataset = CustomDataset(sptimg4_test_norm, tbg4_test_norm)
-            test_dataset_notnorm = CustomDataset(sptimg4_test_notnorm, tbg4_test_notnorm)
         else:
             train_dataset = CustomDataset(sptimg4_train_notnorm, tbg4_train_notnorm)
             test_dataset = CustomDataset(sptimg4_test_notnorm, tbg4_test_notnorm)
-            test_dataset_notnorm = CustomDataset(sptimg4_test_notnorm, tbg4_test_notnorm)
+
+        test_dataset_notnorm = CustomDataset(sptimg4_test_notnorm, tbg4_test_notnorm)
 
     train_loader = DataLoader(train_dataset, batch_size=config.BATCH_SIZE, shuffle=True)
     test_loader = DataLoader(test_dataset, batch_size=config.BATCH_SIZE, shuffle=True)
     test_loader_notnorm = DataLoader(test_dataset_notnorm, batch_size=config.BATCH_SIZE, shuffle=True)
 
-    return train_loader, test_loader, test_loader_notnorm
+    return train_loader, test_loader, test_loader_notnorm, gt_spt_test
 
 
 

main.py
@@ -5,19 +5,11 @@ import config
 import argparse
 import random
 import os
-from utils import get_base_folder_name, get_model_path_filename
+from utils import ignore_warnings, set_seed, get_base_folder_name, get_model_path_filename
 from models import UNet, AttentionUNet
 from dataset import get_train_test_datasets
 from train import train
 from test import test
-
-def set_seed(seed=42):
-    """Ensures reproducibility by setting random seeds."""
-    random.seed(seed)
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-    if torch.cuda.is_available():
-        torch.cuda.manual_seed_all(seed)
 
 def get_models():
     """Initialize the chosen model."""
@@ -33,6 +25,9 @@ def get_models():
     return models
 
 def main():
+
+    ignore_warnings()
+
     parser = argparse.ArgumentParser(description="Train or Test a UNet Model")
     parser.add_argument("--train", action="store_true", help="Train the model")
     parser.add_argument("--test", action="store_true", help="Test the model")
@@ -97,7 +92,7 @@ def main():
         os.chdir("..")
 
     if args.test:
-        _, test_loader, test_loader_notnorm = get_train_test_datasets()
+        _, test_loader, test_loader_notnorm, gt_spt_test = get_train_test_datasets()
 
         base_folder = get_base_folder_name(config.MODEL_TYPE)
         os.chdir(base_folder)
@@ -106,7 +101,7 @@ def main():
         for model_folder in models.keys():
             models[model_folder].load_state_dict(
                 torch.load(os.path.join(model_folder, get_model_path_filename(model_folder)), weights_only=False))
-        test(models, test_loader, test_loader_notnorm)
+        test(models, test_loader, test_loader_notnorm, gt_spt_test)
 
 if __name__ == "__main__":
     main()

test.py
@@ -94,7 +94,7 @@ def save_rmse_figures(dataSets):
     # plt.show()
 
 
-def save_rep_samples(model_name, test_loader_notnorm, YPred, tbg4_test, sptimg4_test):
+def save_rep_samples(model_name, test_loader_notnorm, YPred, tbg4_test, sptimg4_test, gt_spt_test):
     indices = [0, 1, 2, 3, 4]
 
     fig, axes = plt.subplots(5, 5, figsize=(50, 15))
@@ -110,52 +110,63 @@ def save_rep_samples(model_name, test_loader_notnorm, YPred, tbg4_test, sptimg4_
     # Loop to plot all graphs
     for i, idx in enumerate(indices):
         predicted_background = YPred[idx, :, :].transpose(1, 0)
-        ground_truth = tbg4_test[idx, :, :].transpose(1, 0)
-        original_speimg = sptimg4_test[idx, :, :].transpose(1, 0)
-        # ground_truth_spectral_image = gt_spt_test[idx, :, :].transpose(1, 0)
+        ground_truth_background = tbg4_test[idx, :, :].transpose(1, 0)
+        original_spectral_image = sptimg4_test[idx, :, :].transpose(1, 0)
+        ground_truth_spectral_image = gt_spt_test[idx, :, :].transpose(1, 0)
 
-        if config.NORMALIZATION and config.DENORMALIZATION:
-            ground_truth = ground_truth * (tbg4_test_max - tbg4_test_min) + tbg4_test_min
-            predicted_background = predicted_background * (sptimg4_test_max - sptimg4_test_min) + sptimg4_test_min
-            original_speimg = original_speimg * (sptimg4_test_max - sptimg4_test_min) + sptimg4_test_min
+        predicted_spectral_image = original_spectral_image - predicted_background
 
-        predicted_spectral_image = original_speimg - predicted_background
-        ground_truth_spectral_image = original_speimg - ground_truth
-
-        if config.NORMALIZATION and config.DENORMALIZATION:
-            predicted_spectral_image = (predicted_spectral_image - np.min(predicted_spectral_image)) / \
-                                       (np.max(predicted_spectral_image) - np.min(predicted_spectral_image) + 1e-8)
         if idx == 1:
             print("Predicted Background {}".format(idx))
             print("Minimum Intensity: {}".format(np.min(predicted_background)))
             print("Maximum Intensity: {}".format(np.max(predicted_background)))
 
             print("Ground Truth Background {}".format(idx))
-            print("Minimum Intensity: {}".format(np.min(ground_truth)))
-            print("Maximum Intensity: {}".format(np.max(ground_truth)))
+            print("Minimum Intensity: {}".format(np.min(ground_truth_background)))
+            print("Maximum Intensity: {}".format(np.max(ground_truth_background)))
+
+            print("Predicted Spectral Image {}".format(idx))
+            print("Minimum Intensity: {}".format(np.min(predicted_spectral_image)))
+            print("Maximum Intensity: {}".format(np.max(predicted_spectral_image)))
+
+            print("Ground Truth Spectral Image {}".format(idx))
+            print("Minimum Intensity: {}".format(np.min(ground_truth_spectral_image)))
+            print("Maximum Intensity: {}".format(np.max(ground_truth_spectral_image)))
 
             print("Original Spectral Image {}".format(idx))
-            print("Minimum Intensity: {}".format(np.min(original_speimg)))
-            print("Maximum Intensity: {}".format(np.max(original_speimg)))
+            print("Minimum Intensity: {}".format(np.min(original_spectral_image)))
+            print("Maximum Intensity: {}".format(np.max(original_spectral_image)))
 
         ax = axes[i, 0]
-        ax.imshow(predicted_background, aspect='auto', cmap='gray')
+        if config.NORMALIZATION:
+            ax.imshow(predicted_background, aspect='auto', cmap='gray', vmin=0, vmax=1)
+        else:
+            ax.imshow(predicted_background, aspect='auto', cmap='gray')
         ax.set_title(f'Predicted Background {idx}')
 
         ax = axes[i, 1]
-        ax.imshow(ground_truth, aspect='auto', cmap='gray')
+        if config.NORMALIZATION:
+            ax.imshow(ground_truth_background, aspect='auto', cmap='gray', vmin=0, vmax=1)
+        else:
+            ax.imshow(ground_truth_background, aspect='auto', cmap='gray')
         ax.set_title(f'Ground Truth Background {idx}')
 
         ax = axes[i, 2]
-        ax.imshow(predicted_spectral_image, aspect='auto', cmap='gray')
+        if config.NORMALIZATION:
+            ax.imshow(predicted_spectral_image, aspect='auto', cmap='gray')
+        else:
+            ax.imshow(predicted_spectral_image, aspect='auto', cmap='gray')
         ax.set_title(f'Predicted Spectral Image {idx}')
 
         ax = axes[i, 3]
-        ax.imshow(ground_truth_spectral_image, aspect='auto', cmap='gray')
+        if config.NORMALIZATION:
+            ax.imshow(ground_truth_spectral_image, aspect='auto', cmap='gray')
+        else:
+            ax.imshow(ground_truth_spectral_image, aspect='auto', cmap='gray')
         ax.set_title(f'Ground Truth Spectral Image {idx}')
 
         ax = axes[i, 4]
-        ax.imshow(original_speimg, aspect='auto', cmap='gray')
+        ax.imshow(original_spectral_image, aspect='auto', cmap='gray')
         ax.set_title(f'Original Spectral Image {idx}')
 
     plt.tight_layout(rect=[0, 0.03, 1, 0.95], pad=3.0)
@@ -163,10 +174,9 @@ def save_rep_samples(model_name, test_loader_notnorm, YPred, tbg4_test, sptimg4_
         plt.savefig("unet/convUNet_5_rep_samples.png")
     elif model_name == "attention_unet":
         plt.savefig("attention_unet/convUNet_Att_5_rep_samples.png")
-    pass
 
 
-def test(models, test_loader, test_loader_notnorm):
+def test(models, test_loader, test_loader_notnorm, gt_spt_test):
     """Evaluates both models on the test dataset and stores results."""
 
     (numSpectra, sptimg4_test, tbg4_test, YPred, Predictspe,
@@ -207,6 +217,7 @@ def test(models, test_loader, test_loader_notnorm):
     YPred2 = np.squeeze(YPred2, axis=1)
     Predictspe2 = np.squeeze(Predictspe2, axis=1)
     Oldsptimg = np.squeeze(Oldsptimg, axis=1)
+    gt_spt_test = np.squeeze(gt_spt_test, axis=1)
     mean_background = np.squeeze(mean_background, axis=0)
 
     sptn = np.mean(Predictspe[:, :, 6:10], axis=2)
@@ -222,8 +233,6 @@ def test(models, test_loader, test_loader_notnorm):
 
         # Interpolate at the points in xq and store the result in vq
         vq[i, :] = f(xq)
-
-    # print(vq.shape)
 
     sptimg4_test = np.squeeze(sptimg4_test, axis=1)
     rawspt = np.mean(sptimg4_test[:, :, 6:10], axis=2)
@@ -258,29 +267,21 @@ def test(models, test_loader, test_loader_notnorm):
         return np.sqrt(np.mean((predictions - targets) ** 2, axis=(1, 2)))
 
     ConvU_BG_RMSE = rmse(YPred, tbg4_test)
-    print(ConvU_BG_RMSE.shape)
     ConvU_SPE_RMSE = rmse(Predictspe, GTsptimg)
-    print(ConvU_SPE_RMSE.shape)
     ConvU_Att_BG_RMSE = rmse(YPred2, tbg4_test)
-    print(ConvU_Att_BG_RMSE.shape)
     ConvU_Att_SPE_RMSE = rmse(Predictspe2, GTsptimg)
-    print(ConvU_Att_SPE_RMSE.shape)
 
-    print("Mean bg:", mean_background.shape)
     Old_BG = np.tile(mean_background[np.newaxis, :, :], (numSpectra, 1, 1))
-    print(Old_BG.shape)
     Old_BG_RMSE = rmse(Old_BG, tbg4_test)
-    print(Old_BG_RMSE.shape)
     Old_SPE_RMSE = rmse(Oldsptimg, GTsptimg)
-    print(Old_SPE_RMSE.shape)
 
     # Plotting figures
 
     dataSets = [ConvU_Att_BG_RMSE, ConvU_Att_SPE_RMSE, ConvU_BG_RMSE, ConvU_SPE_RMSE, Old_BG_RMSE, Old_SPE_RMSE]
     save_rmse_figures(dataSets)
 
-    print("Conventional UNet")
-    save_rep_samples("unet", test_loader_notnorm, YPred, tbg4_test, sptimg4_test)
+    print("Conventional UNet\n")
+    save_rep_samples("unet", test_loader_notnorm, YPred, tbg4_test, sptimg4_test, gt_spt_test)
 
-    print("Conventional UNet with Attention")
-    save_rep_samples("attention_unet", test_loader_notnorm, YPred2, tbg4_test, sptimg4_test)
+    print("Conventional UNet with Attention\n")
+    save_rep_samples("attention_unet", test_loader_notnorm, YPred2, tbg4_test, sptimg4_test, gt_spt_test)